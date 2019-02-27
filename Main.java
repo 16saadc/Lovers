ab489b1e41ffd567eb8b976beaab20e829cd9339
Will Hay Jr <willhayjr@gmail.com>
2019-02-26 19:44:29 -0500

    new decision logic

--- Main.java
+++ Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
 
         UserState sober_longTerm = new UserState(RelationshipGoal.LONG_TERM, Sobriety.SOBER, MateState.FRIENDS, Environment.INVITING);
+        UserState drunk_Hookup = new UserState(RelationshipGoal.ONE_NIGHT, Sobriety.DRUNK, MateState.FAMILIAR, Environment.INVITING);
+        UserState drunk_Uninviting = new UserState(RelationshipGoal.ONE_NIGHT, Sobriety.DRUNK, MateState.FAMILIAR, Environment.NO_GO);
 
         PhysicalAttraction highPhysicalAttraction = new PhysicalAttraction(Level.HIGH);
         PhysicalAttraction lowPhysicalAttraction = new PhysicalAttraction(Level.LOW);
@@ -24,16 +26,22 @@ public class Main {
 
         PotentialMate attractiveMate = new PotentialMate(goodDate, "David");
         PotentialMate semiAttractive = new PotentialMate(goodHookup, "Katie");
-        PotentialMate notAttractive = new PotentialMate(low, "Vish");
+        PotentialMate notAttractive = new PotentialMate(low, "Paul");
 
 
         MateFinder mateFinder = new MateFinder(attractiveMate, sober_longTerm);
+        MateFinder mateFinder2 = new MateFinder(semiAttractive, drunk_Hookup);
+        MateFinder mateFinder3 = new MateFinder(notAttractive, drunk_Uninviting);
 
 
-        //very drunk and not looking for long term
+        //sober, friends with mate, inviting environment, looking for long term, attractive mate:
         System.out.println(mateFinder.getPotentialMate().getName() + ": " + mateFinder.decide());
-        //sober and looking for long term relationship
-        //System.out.println(mateFinder.decide(10, true).getName());
+
+        //drunk, not looking for long term, inviting environment, attractive mate:
+        System.out.println(mateFinder2.getPotentialMate().getName() + ": " + mateFinder2.decide());
+
+        //drunk, not looking for long term, not a good environment, unattractive mate
+        System.out.println(mateFinder3.getPotentialMate().getName() + ": " + mateFinder3.decide());
 
     }
 }
--- MateFinder.java
+++ MateFinder.java
@@ -38,6 +38,13 @@ public class MateFinder {
         return currentState;
     }
 
+    public Level getPotentialMatePersonality() {
+        return potentialMate.getAttraction().getPersonalityAttraction().getPersonalityLevel();
+    }
+
+    public Level getPotentialMatePhysicalAttractiveness() {
+        return potentialMate.getAttraction().getPhysicalAttraction().getLevel();
+    }
     /**
      * methods:
      * assess the candidate's state
@@ -47,10 +54,135 @@ public class MateFinder {
      */
 
     public Move decide() {
-
-        if (currentState.getSobriety().equals(Sobriety.SOBER)) {
-            return Move.ASK_OUT;
+        Level potentialMatePersonality = getPotentialMatePersonality();
+        Level potentialMatePhysical = getPotentialMatePhysicalAttractiveness();
+        
+        //userState is mostly sober
+        if (currentState.getSobriety().equals(Sobriety.SOBER) || currentState.getSobriety().equals(Sobriety.TIPSY)) {
+            //looking for long term
+            if (currentState.getRelationshipGoal().equals(RelationshipGoal.LONG_TERM)) {
+                if ((potentialMatePersonality.equals(Level.HIGH) || potentialMatePersonality.equals(Level.MEDIUM)) 
+                && potentialMatePhysical.equals(Level.HIGH) || potentialMatePhysical.equals(Level.MEDIUM)) {
+                    //high personality/physical attractiveness
+                    if (!currentState.getEnvironment().equals(Environment.NO_GO)) {
+                        //attractive person, looking for long term, environment is perfect, make your move
+                        if (currentState.getEnvironment().equals(Environment.DEFINITELY)) {
+                            return Move.ASK_OUT;
+                        }
+                        if (currentState.getMateState().equals(MateState.ALREADY_CRUSH)) {
+                            if (currentState.getEnvironment().equals(Environment.INVITING)) {
+                                return Move.ASK_OUT;
+                            }
+                        } else if (currentState.getMateState().equals(MateState.FRIENDS)) {
+                            if (currentState.getEnvironment().equals(Environment.INVITING)) {
+                                return Move.FLIRT;
+                            }
+                        }
+                        //barely know the person, environment isn't perfect, talk to them more 
+                        else {
+                            return Move.TALK_MORE;
+                        }
+                    }
+                    //environment is a no go so don't approach
+                    return Move.DO_NOT_APPROACH;
+                }
+                return Move.ASK_OUT;
+            } else if (currentState.getRelationshipGoal().equals(RelationshipGoal.ONE_NIGHT)) {
+                //dont take into account personality for one night goal
+                if (potentialMatePhysical.equals(Level.HIGH) || potentialMatePhysical.equals(Level.MEDIUM)) {
+                    if (!currentState.getEnvironment().equals(Environment.NO_GO)) {
+                        if (currentState.getEnvironment().equals(Environment.BE_CONFIDENT)) {
+                            return Move.TALK_MORE;
+                        } else {
+                            return Move.FLIRT;
+                        }
+                    }
+                    return Move.DO_NOT_APPROACH;
+                }
+            } else {
+                //unsure of relationship goal
+                if ((potentialMatePersonality.equals(Level.HIGH) || potentialMatePersonality.equals(Level.MEDIUM)) 
+                && potentialMatePhysical.equals(Level.HIGH) || potentialMatePhysical.equals(Level.MEDIUM)) {
+                    if (!currentState.getEnvironment().equals(Environment.NO_GO)) {
+                        return Move.TALK_MORE;
+                    }
+                    return Move.DO_NOT_APPROACH;
+                }
+                else {
+                    return Move.DO_NOT_APPROACH;
+                }
+                
+            }
+            
         }
+        //userState is intoxicated  
+        else {
+            //user is drunk
+            if (currentState.getSobriety().equals(Sobriety.DRUNK)) {
+                 //looking for long term
+                if (currentState.getRelationshipGoal().equals(RelationshipGoal.LONG_TERM)) {
+                    if ((potentialMatePersonality.equals(Level.HIGH) || potentialMatePersonality.equals(Level.MEDIUM)) 
+                    && potentialMatePhysical.equals(Level.HIGH) || potentialMatePhysical.equals(Level.MEDIUM)) {
+                        //high personality/physical attractiveness
+                        if (!currentState.getEnvironment().equals(Environment.NO_GO)) {
+                            //attractive person, looking for long term, environment is perfect, make your move
+                            if (currentState.getEnvironment().equals(Environment.DEFINITELY)) {
+                                return Move.ASK_OUT;
+                            }
+                            if (currentState.getMateState().equals(MateState.ALREADY_CRUSH)) {
+                                if (currentState.getEnvironment().equals(Environment.INVITING)) {
+                                    return Move.ASK_OUT;
+                                }
+                            } 
+                            //environment is good, userState is drunk, flirt more
+                            if (currentState.getEnvironment().equals(Environment.INVITING)) {
+                                return Move.FLIRT;
+                            }
+                            //environment isn't perfect, talk to them more 
+                            else {
+                                return Move.TALK_MORE;
+                            }
+                        }
+                        //environment is a no go so don't approach
+                        return Move.DO_NOT_APPROACH;
+                    }
+                    return Move.ASK_OUT;
+                } else if (currentState.getRelationshipGoal().equals(RelationshipGoal.ONE_NIGHT)) {
+                    //dont take into account personality for one night goal
+                    if (potentialMatePhysical.equals(Level.HIGH) || potentialMatePhysical.equals(Level.MEDIUM)) {
+                        if (!currentState.getEnvironment().equals(Environment.NO_GO)) {
+                                //environment is not terrible, looking for one night, attractive potential mate, flirt more
+                                return Move.FLIRT;
+                            }
+                        }
+                        return Move.DO_NOT_APPROACH;
+                } else {
+                    //unsure of relationship goal
+                    if ((potentialMatePersonality.equals(Level.HIGH) || potentialMatePersonality.equals(Level.MEDIUM)) 
+                    && potentialMatePhysical.equals(Level.HIGH) || potentialMatePhysical.equals(Level.MEDIUM)) {
+                        //environment is bad but userState is drunk, so talk more
+                        if (currentState.getEnvironment().equals(Environment.NO_GO)) {
+                            return Move.TALK_MORE;
+                        } else {
+                            return Move.FLIRT;
+                        }
+                    }
+                    else {
+                        return Move.DO_NOT_APPROACH;
+                    }
+                    
+                }
+            }
+            //disregard most social cues when extremely inebriated
+            if (currentState.getSobriety().equals(Sobriety.BLACKOUT)) {
+                if (currentState.getEnvironment().equals(Environment.NO_GO)) {
+                    return Move.TALK_MORE;
+                } else {
+                    return Move.FLIRT;
+                }
+            }
+        }
+        
         return Move.DO_NOT_APPROACH;
 
 
